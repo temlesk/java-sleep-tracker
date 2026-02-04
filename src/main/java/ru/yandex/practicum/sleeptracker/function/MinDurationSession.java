e708e3f48d108e35f3ea2e00891b849980c7b9d8
Temlesk <Teml1010>
2026-02-04 21:34:39 +0300

    commit 9, CheckStyle

--- src/main/java/ru/yandex/practicum/sleeptracker/function/MinDurationSession.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/function/MinDurationSession.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
 
-public class MinDurationSession implements Function <List<SleepingSession> , SleepAnalysisResult> {
+public class MinDurationSession implements Function<List<SleepingSession>, SleepAnalysisResult> {
     public static final String TITLE = "Минимальная продолжительность сессии (в минутах)";
 
     @Override
